b375066614926a4a16a6446c2354f73856ce1394
Tariq5mo <tariqomer756@gmail.com>
2024-11-24 11:58:20 +0200

    Add task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,2 +1,15 @@
-import pymongo
-help(pymongo)
+"""
+Module for listing all documents in a MongoDB collection.
+"""
+
+def list_all(mongo_collection):
+    """Lists all documents in a collection.
+
+    Args:
+        mongo_collection: The pymongo collection object.
+
+    Returns:
+        A list of all documents in the collection, or an empty list if no documents are found.
+    """
+    result = mongo_collection.find()
+    return list(result) if result else []
