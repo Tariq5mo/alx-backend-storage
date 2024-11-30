9e8b4891abe6b264ec2afe86822f4a022f5513cc
Tariq5mo <tariqomer756@gmail.com>
2024-11-30 14:24:09 +0200

    Add task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 This module defines a Cache class to interact with a Redis database.
 """
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 from uuid import uuid4
 
 
@@ -32,3 +32,56 @@ class Cache:
         random_key = uuid4()
         self._redis.set(str(random_key), data)
         return str(random_key)
+
+    # Task 2
+
+    def get(
+        self,
+        key: str,
+        fn: Optional[Callable[[bytes], Union[str, bytes, int, float]]] = None,
+    ) -> Union[str, bytes, int, float]:
+        """
+        Retrieve data from Redis and optionally apply a conversion function.
+
+        Args:
+            key (str): The key to retrieve from Redis.
+            fn (Optional[Callable[[bytes], Union[str, bytes, int, float]]],
+                                                                    optional):
+                A function to convert the data back to the desired format.
+                                                            Defaults to None.
+
+        Returns:
+            Union[str, bytes, int, float]: The retrieved data,
+                                        optionally converted by fn.
+        """
+        result = self._redis.get(key)
+        if result is None:
+            return None
+        if fn:
+            return fn(result)
+        else:
+            return result
+
+    def get_str(self, key: str) -> str:
+        """
+        Retrieve a string from Redis.
+
+        Args:
+            key (str): The key to retrieve from Redis.
+
+        Returns:
+            str: The retrieved string.
+        """
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieve an integer from Redis.
+
+        Args:
+            key (str): The key to retrieve from Redis.
+
+        Returns:
+            int: The retrieved integer.
+        """
+        return self.get(key, int)
